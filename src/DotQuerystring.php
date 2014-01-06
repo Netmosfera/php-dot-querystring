021d81d0233cadfdc2387db2be9ef6ead0bb8aaa
WesNetmo <netmosfera@gmail.com>
2014-01-06 03:18:46 +0100

    Update DotQuerystring.php

--- src/DotQuerystring.php
+++ src/DotQuerystring.php
@@ -2,15 +2,29 @@
 
 final class DotQueryString
 {
-	static function decode($querystring){
+	/**
+	 * @param string
+	 * @return array
+	 */
+	static function decode($querystring)
+	{
 		$bracketSyntax = preg_replace('/[.]([^.]*?)(?=[=.&]|$)/', '%5B\1%5D', $querystring);
 		parse_str($bracketSyntax, $array);
 		return $array;
 	}
 
-	static function encode($data, $numericPrefix = "", $separator = NULL, $encoding = NULL){
-		$encoding = $encoding === NULL ? PHP_QUERY_RFC1738 : $encoding;
-		$separator = $separator === NULL ? ini_get("arg_separator.output") : $separator;
+	/**
+	 * @param array|object
+	 * @param string
+	 * @param string|null When NULL is provided, ini value arg_separator.output is used
+	 * @param int|null When NULL is provided PHP_QUERY_RFC1738 is used
+	 * @return string
+	 * @link http://php.net/http_build_query
+	 */
+	static function encode($data, $numericPrefix = '', $separator = null, $encoding = null)
+	{
+		$encoding = $encoding ? $encoding : PHP_QUERY_RFC1738;
+		$separator = $separator ? $separator : ini_get('arg_separator.output');
 		$data = http_build_query($data, $numericPrefix, $separator, $encoding);
 		return preg_replace('/%5B(.*?)%5D/', '.\1', $data);
 	}
