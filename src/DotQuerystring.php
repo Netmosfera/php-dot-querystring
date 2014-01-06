de11eed9e09cd8f26b2ec04e662b82e3d1b8e937
WesNetmo <netmosfera@gmail.com>
2014-01-06 02:54:42 +0100

    Update DotQuerystring.php

--- src/DotQuerystring.php
+++ src/DotQuerystring.php
@@ -2,15 +2,16 @@
 
 final class DotQueryString
 {
-  static function decode($querystring)
-  {
-    $bracketSyntax = preg_replace('/[.]([^.]*?)(?=[=.&]|$)/', '%5B\1%5D', $dotSyntax);
-    parse_str($bracketSyntax, $array);
-    return $array;
-  }
-  
-  static function encode($data, $numericPrefix = NULL, $separator = NULL, $encoding = NULL){
-    $data = http_build_query($data, $numericPrefix, $separator, $encoding);
-    return preg_replace('/%5B(.*?)%5D/', '.\1', $data);
-  }
+	static function decode($querystring){
+		$bracketSyntax = preg_replace('/[.]([^.]*?)(?=[=.&]|$)/', '%5B\1%5D', $querystring);
+		parse_str($bracketSyntax, $array);
+		return $array;
+	}
+
+	static function encode($data, $numericPrefix = "", $separator = NULL, $encoding = NULL){
+		$encoding = $encoding === NULL ? PHP_QUERY_RFC1738 : $encoding;
+		$separator = $separator === NULL ? ini_get("arg_separator.output") : $separator;
+		$data = http_build_query($data, $numericPrefix, $separator, $encoding);
+		return preg_replace('/%5B(.*?)%5D/', '.\1', $data);
+	}
 }
